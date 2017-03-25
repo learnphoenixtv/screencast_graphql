074ad5f1d00a710582b1f2024ef8fb91effb8bb5
Daniel Berkompas <himself@danielberkompas.com>
2017-03-25 12:54:24 -0700

    Fix N+1 query

--- lib/screencast/graphql/resolvers/episode_resolver.ex
+++ lib/screencast/graphql/resolvers/episode_resolver.ex
@@ -1,16 +1,27 @@
 defmodule Screencast.GraphQL.EpisodeResolver do
+  import Absinthe.Resolution.Helpers
+  import Ecto.Query
+
   alias Screencast.{
     Episode,
     Repo
   }
 
-  def assoc(_args, %{source: series}) do
-    series = Repo.preload(series, :episodes)
-    {:ok, series.episodes}
+  def assoc(_args, %{source: series} = context) do
+    batch {__MODULE__, :by_series_id}, series.id, fn results ->
+      {:ok, Map.get(results, series.id)}
+    end
   end
 
   def create(%{id: id, title: title}, _context) do
     episode = Repo.insert! %Episode{series_id: id, title: title}
     {:ok, episode}
+  end
+
+  def by_series_id(_, ids) do
+    Episode
+    |> where([s], s.series_id in ^ids)
+    |> Repo.all
+    |> Enum.group_by(&(&1.series_id))
   end
 end
